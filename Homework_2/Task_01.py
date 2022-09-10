50c04c4330a96f73a57bcb6aee87e99d4f4a8728
Ulya <Gerony690@yandex.ru>
2022-09-10 10:56:04 +0300

    Task_01_HW2

---
+++ Homework_2/Task_01.py
@@ -0,0 +1,22 @@
+# Напишите программу, которая принимает на вход вещественное число
+# и показывает сумму его цифр.
+# Пример:
+# - 6782 -> 23
+# - 0,56 -> 11
+
+real_number = input('Введите вещественное число: ')
+
+def sum_of_digits (num):
+    x = num.split(".") 
+    a = int(x[0]) 
+    b = int(x[1]) 
+    sum = 0
+    while (a != 0): 
+        sum = sum + a % 10
+        a = a // 10
+    while (b != 0): 
+        sum = sum + b % 10
+        b = b // 10
+    print(f'Сумма цифр равна:{sum}')
+
+sum_of_digits(real_number)
