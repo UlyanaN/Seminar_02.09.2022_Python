ecebfaeac6c2de5870c9b94db9e36df1da876411
Ulya <Gerony690@yandex.ru>
2022-09-10 12:23:51 +0300

    Task_03_HW2

--- Homework_2/Task_03.py
+++ Homework_2/Task_03.py
@@ -3,5 +3,16 @@
 # Пример:
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-number = int(input('Введите целое число: '))
+# Последовательность (1+1/n)**n
 
+number = int(input('Введите число: '))
+
+def sum_of_sub (n):
+    sum = 0
+    list = {}
+    for i in range (1, n+1):
+        sum+=(1+1/i)**i
+        list [i] = sum
+    print (list)
+
+sum_of_sub (number)
