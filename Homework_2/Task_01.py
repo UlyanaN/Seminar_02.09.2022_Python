0ff6a2ba308fcc09994a4b098686a7c3ad44fe44
Ulya <Gerony690@yandex.ru>
2022-09-10 11:54:51 +0300

    New changes_2 Task_01_HW2

--- Homework_2/Task_01.py
+++ Homework_2/Task_01.py
@@ -12,13 +12,23 @@ def sum_of_digits (num):
     if a < 0:
         a*=-1
     b = int(x[1]) 
-    sum = 0
+    suma = 0
     while (a != 0): 
-        sum = sum + a % 10
+        suma = suma + a % 10
         a = a // 10
     while (b != 0): 
-        sum = sum + b % 10
+        suma = suma + b % 10
         b = b // 10
-    print(f'Сумма цифр равна:{sum}')
+    print(f'Сумма цифр равна:{suma}')
 
 sum_of_digits(real_number)
+
+# def sum_of_d2 (number):
+#     summ = 0
+#     for i in number:
+#         if i.isdigit():
+#             summ = summ + int(i)
+#     print(f'Сумма цифр равна:{summ}')
+
+
+# sum_of_d2 (real_number)
