8a6fc06066d1ec47289e2ac4b611b6a76dcd288b
Ulya <Gerony690@yandex.ru>
2022-09-10 12:36:44 +0300

    New change_Task_03_HW2

--- Homework_2/Task_03.py
+++ Homework_2/Task_03.py
@@ -1,4 +1,4 @@
-# Задайте список из n чисел последовательности 
+# Задайте список из n чисел последовательности
 # $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 # Пример:
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
@@ -7,12 +7,17 @@
 
 number = int(input('Введите число: '))
 
-def sum_of_sub (n):
-    sum = 0
-    list = {}
-    for i in range (1, n+1):
-        sum+=(1+1/i)**i
-        list [i] = sum
-    print (list)
 
-sum_of_sub (number)
+def sum_of_sub(n):
+    sam = 0
+    list = {}
+    result = 0
+    for i in range(1, n+1):
+        sam = (1+1/i)**i
+        list[i] = sam
+        result += sam
+    print(list)
+    print(result)
+
+
+sum_of_sub(number)
