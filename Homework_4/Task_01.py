b25d11ae2a17ec203693996a75b9e456a4195e17
Ulya <Gerony690@yandex.ru>
2022-09-17 16:54:35 +0300

    Task_01_HW4

--- Homework_4/Task_01.py
+++ Homework_4/Task_01.py
@@ -1,3 +1,24 @@
 # Вычислить число c заданной точностью d
 # Пример:
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+import math
+
+
+snumber = (input('Введите точность в виде дробного числа: '))
+
+if '.' in snumber:
+    d = len(snumber[snumber.index('.') + 1:]) 
+elif ',' in snumber:
+    d = len(snumber[snumber.index(',') + 1:])
+else:
+    print('Введены некорректные данные')
+
+
+# сокруглением
+print(round(math.pi, d))
+
+print('-'*10)
+
+# без округления
+print(str(math.pi)[:d+2])
