b47238a5d8d4b044ed574430eaa3955f1b783ebe
Ulya <Gerony690@yandex.ru>
2022-09-19 17:43:34 +0300

    Task_04HW4

--- Homework_4/Task_04.py
+++ Homework_4/Task_04.py
@@ -4,4 +4,43 @@
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-number = int(input('Введите натуральную степень: '))
+from random import randint
+
+degree_k = int(input('Введите натуральную степень: '))
+
+# nn - число элементов, d - степень
+
+
+def coeff(nn):
+    list_of_coef = []
+    i = 0
+    while i < nn:
+        c = randint(0, 101)
+        list_of_coef.append(c)
+        i += 1
+    return list_of_coef
+
+
+def create_poly(list, d):
+    newlist = []
+    for i in range(len(list)-1):
+        if i == 0:
+            newlist.append(str(list[i]) + 'x^' + str(d))
+        else:
+            newlist.append(str(list[i]) + 'x')
+    newlist.append(str(list[-1]))
+    return (newlist)
+
+
+if degree_k < 1:
+    print("Неверно указана степень")
+else:
+    n = randint(2, 5)
+    print(n)
+    list1 = coeff(n)
+    print(list1)
+    list_sec = create_poly(list1, degree_k)
+    print('+'.join(list_sec))
+
+with open('Polynomial_Task)4_HW4.txt', 'w') as data:
+    data.write('+'.join(list_sec))
